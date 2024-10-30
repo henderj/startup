3a5a4a69befa3cd06f4c5f52f38643b359ede481
Josh Hendershot <hendershotjoshr@gmail.com>
2024-10-30 16:40:41 -0600

    feat: initial app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -2,5 +2,32 @@ import React from 'react';
 import './app.css';
 
 export default function App() {
-  return <div>App will display here</div>;
+  return (
+    <>
+      <header>
+        <h1>QuikVote</h1>
+        <nav>
+          <ul>
+            <li>
+              <a href="./pages/history.html">
+                <span class="material-symbols-outlined">history</span>
+              </a>
+            </li>
+            <li>
+              <a href="./pages/login.html">
+                <span class="material-symbols-outlined">account_circle</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        App component goes here
+      </main>
+      <footer>
+        <span class="text-reset">Josh Hendershot</span>
+        <a href="https://github.com/henderj/startup">Github</a>
+      </footer>
+    </>
+  )
 }
