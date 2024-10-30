1de7d5f8fcea60e7822a19afa3acd56ba2fb7c52
Josh Hendershot <hendershotjoshr@gmail.com>
2024-10-30 16:55:41 -0600

    feat: initial home.jsx

---
+++ src/home/home.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+export default function Home() {
+  return (
+    <>
+      <a class="button" href="./pages/new.html">New QuikVote</a>
+      <a class="button" href="./pages/join.html">Join QuikVote</a>
+      <p><a href="./pages/login.html">Login/Register</a> to see your past QuikVotes</p>
+    </>
+  )
+}
