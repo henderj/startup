e7798997ffa62339bf0d02358eea18c036f6a9b7
Josh Hendershot <hendershotjoshr@gmail.com>
2024-10-31 18:54:54 -0600

    feat: initial results.jsx

---
+++ src/results/results.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './results.css';
+
+/*
+  <title>Results</title>
+  ...
+  <header>
+    <h1>Results</h1>
+  </header>
+  */
+
+export default function Results() {
+  return (
+    <>
+      <ol>
+        <li>Pizza</li>
+        <li>Burgers</li>
+        <li>Seafood</li>
+      </ol>
+      <a class="button" href="../index.html">Home</a>
+    </>
+  )
+}
