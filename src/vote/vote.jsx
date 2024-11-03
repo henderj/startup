18cd8b655104d4cf5267ce30ea1d0d1abaeb46e6
Josh Hendershot <hendershotjoshr@gmail.com>
2024-11-03 14:40:20 -0700

    fix: css selectors and use NavLink and className

--- src/vote/vote.jsx
+++ src/vote/vote.jsx
@@ -1,60 +1,61 @@
 import React from 'react';
 import './vote.css';
+import { NavLink } from 'react-router-dom';
 
 /*
   <title>QuikVote</title>
   ...
     <h3>YBD-027</h3>
-    <span class="material-symbols-outlined">content_copy</span>
+    <span className="material-symbols-outlined">content_copy</span>
   */
 
 export default function Vote() {
   return (
     <>
-      <ul>
-        <li>Pizza
-          <div class="vote-buttons">
-            <button>
-              <span class="material-symbols-outlined">arrow_upward</span>
+      <ul className="vote-options">
+        <li className="vote-options__item">Pizza
+          <div className="vote-buttons">
+            <button className="vote-buttons__button">
+              <span className="material-symbols-outlined">arrow_upward</span>
             </button>
-            <span class="vote-value">2</span>
-            <button>
-              <span class="material-symbols-outlined">arrow_downward</span>
-            </button>
-          </div>
-        </li>
-        <li>Burgers
-          <div class="vote-buttons">
-            <button>
-              <span class="material-symbols-outlined">arrow_upward</span>
-            </button>
-            <span class="vote-value">8</span>
-            <button>
-              <span class="material-symbols-outlined">arrow_downward</span>
+            <span className="vote-buttons__value">2</span>
+            <button className="vote-buttons__button">
+              <span className="material-symbols-outlined">arrow_downward</span>
             </button>
           </div>
         </li>
-        <li>Seafood
-          <div class="vote-buttons">
-            <button>
-              <span class="material-symbols-outlined">arrow_upward</span>
+        <li className="vote-options__item">Burgers
+          <div className="vote-buttons">
+            <button className="vote-buttons__button">
+              <span className="material-symbols-outlined">arrow_upward</span>
             </button>
-            <span class="vote-value">5</span>
-            <button>
-              <span class="material-symbols-outlined">arrow_downward</span>
+            <span className="vote-buttons__value">8</span>
+            <button className="vote-buttons__button">
+              <span className="material-symbols-outlined">arrow_downward</span>
+            </button>
+          </div>
+        </li>
+        <li className="vote-options__item">Seafood
+          <div className="vote-buttons">
+            <button className="vote-buttons__button">
+              <span className="material-symbols-outlined">arrow_upward</span>
+            </button>
+            <span className="vote-buttons__value">5</span>
+            <button className="vote-buttons__button">
+              <span className="material-symbols-outlined">arrow_downward</span>
             </button>
           </div>
         </li>
       </ul>
-      <form>
-        <input type="text" placeholder="Add to list" />
-        <button type="submit">
-          <span class="material-symbols-outlined">add</span>
+      <form className="add-option">
+        <input className="add-option__input" type="text" placeholder="Add to list" />
+        <button className="add-option__button" type="submit">
+          <span className="material-symbols-outlined">add</span>
         </button>
       </form>
-      <button class="button">Lock in vote</button>
-      <button class="button">Close vote</button>
-      <a class="button" href="results.html">View Results</a>
+      <button className="main__button">Lock in vote</button>
+      <button className="main__button">Close vote</button>
+      <NavLink className="main__button" to="/results">View Results</NavLink>
     </>
   )
 }
