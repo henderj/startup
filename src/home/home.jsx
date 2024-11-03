bbb0a04dd17924ef5a42a3aabca3787475df0943
Josh Hendershot <hendershotjoshr@gmail.com>
2024-11-03 14:39:09 -0700

    fix: use NavLink and className

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,12 +1,15 @@
 
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export default function Home() {
   return (
     <>
-      <a class="button" href="./pages/new.html">New QuikVote</a>
-      <a class="button" href="./pages/join.html">Join QuikVote</a>
-      <p><a href="./pages/login.html">Login/Register</a> to see your past QuikVotes</p>
+      <NavLink className="main__button" to="new">New QuikVote</NavLink>
+      <NavLink className="main__button" to="join">Join QuikVote</NavLink>
+      <p className="home__p">
+        <NavLink className="home__link" to="login">Login/Register</NavLink> to see your past QuikVotes
+      </p>
     </>
   )
 }
