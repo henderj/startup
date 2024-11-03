846b24ff22d045fd7e5fb117b28922e6e8bc34ef
Josh Hendershot <hendershotjoshr@gmail.com>
2024-11-03 15:00:19 -0700

    fix: NavLink to reference

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -5,10 +5,10 @@ import { NavLink } from 'react-router-dom';
 export default function Home() {
   return (
     <>
-      <NavLink className="main__button" to="new">New QuikVote</NavLink>
-      <NavLink className="main__button" to="join">Join QuikVote</NavLink>
+      <NavLink className="main__button" to="/new">New QuikVote</NavLink>
+      <NavLink className="main__button" to="/join">Join QuikVote</NavLink>
       <p className="home__p">
-        <NavLink className="home__link" to="login">Login/Register</NavLink> to see your past QuikVotes
+        <NavLink className="home__link" to="/login">Login/Register</NavLink> to see your past QuikVotes
       </p>
     </>
   )
