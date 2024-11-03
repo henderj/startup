656e2d7bf6c201cdfbbac002fca867dcfc6a413e
Josh Hendershot <hendershotjoshr@gmail.com>
2024-11-03 14:57:32 -0700

    fix: history page css

--- src/history/history.jsx
+++ src/history/history.jsx
@@ -6,7 +6,7 @@ import './history.css';
   ...
         <li>
           <a href="../index.html">
-            <span class="material-symbols-outlined">arrow_back</span>
+            <span className="material-symbols-outlined">arrow_back</span>
           </a>
         </li>
     <h1>History</h1>
@@ -15,35 +15,27 @@ import './history.css';
 export default function History() {
   return (
     <>
-      <h3>Username: <b>noobmaster42</b></h3>
-      <ol>
-        <li>
-          <div>
-            <h3>Winner: Star Wars</h3>
-            <p>Runner up(s): Lord of the Rings, Nacho Libre, (3 more)</p>
-            <p>Saturday, Sept 19, 2024 - 7:09 PM</p>
-          </div>
+      <h3 className="username">Username: <b>noobmaster42</b></h3>
+      <ol className="history-list">
+        <li className="history-item">
+          <h3 className="history-item__header">Winner: Star Wars</h3>
+          <p className="history-item__content">Runner up(s): Lord of the Rings, Nacho Libre, (3 more)</p>
+          <p className="history-item__content">Saturday, Sept 19, 2024 - 7:09 PM</p>
         </li>
-        <li>
-          <div>
-            <h3>Winner: Secret Hitler</h3>
-            <p>Runner up(s): Codenames, Exploding Kittens, (7 more)</p>
-            <p>Thursday, Aug 22, 2024 - 6:13 PM</p>
-          </div>
+        <li className="history-item">
+          <h3 className="history-item__header">Winner: Secret Hitler</h3>
+          <p className="history-item__content">Runner up(s): Codenames, Exploding Kittens, (7 more)</p>
+          <p className="history-item__content">Thursday, Aug 22, 2024 - 6:13 PM</p>
         </li>
-        <li>
-          <div>
-            <h3>Winner: Dogs</h3>
-            <p>Runner up(s): Cats</p>
-            <p>Monday, June 3, 2024 - 11:51 AM</p>
-          </div>
+        <li className="history-item">
+          <h3 className="history-item__header">Winner: Dogs</h3>
+          <p className="history-item__content">Runner up(s): Cats</p>
+          <p className="history-item__content">Monday, June 3, 2024 - 11:51 AM</p>
         </li>
-        <li>
-          <div>
-            <h3>Winner: Dogs</h3>
-            <p>Runner up(s): Cats</p>
-            <p>Monday, June 3, 2024 - 11:51 AM</p>
-          </div>
+        <li className="history-item">
+          <h3 className="history-item__header">Winner: Dogs</h3>
+          <p className="history-item__content">Runner up(s): Cats</p>
+          <p className="history-item__content">Monday, June 3, 2024 - 11:51 AM</p>
         </li>
       </ol>
     </>
