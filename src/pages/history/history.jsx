f7577ba9030da65963f5770e54f0ad81dc0ba9d4
Josh Hendershot <hendershotjoshr@gmail.com>
2024-11-04 10:34:35 -0700

    feat: history item component

--- src/pages/history/history.jsx
+++ src/pages/history/history.jsx
@@ -1,12 +1,65 @@
 import React from 'react';
 import './history.css';
 import { NavLink } from 'react-router-dom';
+import dayjs from 'dayjs'
 
 /*
   <title>Past QuikVotes</title>
   */
 
+/*
+* data = {
+    winner: string
+    runnersUp: string[]
+    date: datetime
+  }
+*/
+function HistoryItem(props) {
+  const { winner, runnersUp, date } = props.data
+  function getRunnersUp() {
+    const MAX_LENGTH = 3
+    if (runnersUp.length <= MAX_LENGTH) {
+      return runnersUp.join(', ')
+    }
+    const clippedList = runnersUp.slice(0, MAX_LENGTH - 1)
+    clippedList.push(`(${runnersUp.length - MAX_LENGTH + 1} more)`)
+    return clippedList.join(', ')
+  }
+  function getFormattedDate() {
+    return dayjs(date).format('dddd, MMM D, YYYY - h:mm A')
+  }
+  return (
+    <li className="history-item">
+      <h3 className="history-item__header">Winner: {winner}</h3>
+      <p className="history-item__content">Runner up(s): {getRunnersUp()}</p>
+      <p className="history-item__content">{getFormattedDate()}</p>
+    </li>
+  )
+}
+
 export default function History() {
+  const dataArray = [ // TODO: load from server
+    {
+      winner: 'Star Wars',
+      runnersUp: ['Lord of the Rings', 'Nacho Libre', 'Pirates of the Carribean', 'Legally Blond', 'The Notebook'],
+      date: '2024-09-19T19:09:00'
+    },
+    {
+      winner: 'Secret Hitler',
+      runnersUp: ['Codenames', 'Exploding Kittens', 'Game 3', 'Game 4', 'Game 5', 'Game 6', 'Game 7'],
+      date: '2024-08-22T18:13:00'
+    },
+    {
+      winner: 'Dogs',
+      runnersUp: ['Cats'],
+      date: '2024-06-03T11:51:00'
+    }
+  ]
+  function renderItems() {
+    return dataArray.map((data, i) => (
+      <HistoryItem key={i} data={data} />
+    ))
+  }
   return (
     <>
       <header className="header header--center-with-back">
@@ -24,26 +77,7 @@ export default function History() {
       <main className="main">
         <h3 className="username">Username: <b>noobmaster42</b></h3>
         <ol className="history-list">
-          <li className="history-item">
-            <h3 className="history-item__header">Winner: Star Wars</h3>
-            <p className="history-item__content">Runner up(s): Lord of the Rings, Nacho Libre, (3 more)</p>
-            <p className="history-item__content">Saturday, Sept 19, 2024 - 7:09 PM</p>
-          </li>
-          <li className="history-item">
-            <h3 className="history-item__header">Winner: Secret Hitler</h3>
-            <p className="history-item__content">Runner up(s): Codenames, Exploding Kittens, (7 more)</p>
-            <p className="history-item__content">Thursday, Aug 22, 2024 - 6:13 PM</p>
-          </li>
-          <li className="history-item">
-            <h3 className="history-item__header">Winner: Dogs</h3>
-            <p className="history-item__content">Runner up(s): Cats</p>
-            <p className="history-item__content">Monday, June 3, 2024 - 11:51 AM</p>
-          </li>
-          <li className="history-item">
-            <h3 className="history-item__header">Winner: Dogs</h3>
-            <p className="history-item__content">Runner up(s): Cats</p>
-            <p className="history-item__content">Monday, June 3, 2024 - 11:51 AM</p>
-          </li>
+          {renderItems()}
         </ol>
       </main>
     </>
