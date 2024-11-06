854ec7c648c44e37b58b5dd5265852feeb5966a2
Josh Hendershot <hendershotjoshr@gmail.com>
2024-11-05 21:04:53 -0700

    feat: minvote is 1 and value width is fixed

--- src/pages/vote/vote.jsx
+++ src/pages/vote/vote.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './vote.css';
 import { NavLink } from 'react-router-dom';
 
-const MIN_VALUE = 0
+const MIN_VALUE = 1
 const MAX_VALUE = 10
 
 function VoteOption(props) {
