057662a9c30f368fc4134a4aa470514e7b1f906d
Josh Hendershot <hendershotjoshr@gmail.com>
2024-11-13 11:43:06 -0700

    feat: register endpoint

--- service/index.js
+++ service/index.js
@@ -1,10 +1,38 @@
 const express = require('express');
+const uuid = require('uuid');
 const app = express();
+
+const users = {};
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
-app.get('*', (_req, res) => {
-  res.send({ msg: 'QuikVote service' });
+app.use(express.json());
+
+const apiRouter = express.Router();
+app.use('/api', apiRouter);
+
+apiRouter.post('/auth', async (req, res) => {
+  let user = users[req.body.email];
+  if (user) {
+    res.status(409).send({ msg: 'Existing user' });
+    return
+  }
+  if (!req.body.email) {
+    res.status(400).send('Bad Request: Missing email')
+    return
+  }
+  if (!req.body.username) {
+    res.status(400).send('Bad Request: Missing username')
+    return
+  }
+  if (!req.body.password) {
+    res.status(400).send('Bad Request: Missing password')
+    return
+  }
+  user = { email: req.body.email, password: req.body.password, token: uuid.v4() };
+  users[user.email] = user;
+
+  res.send({ token: user.token });
 });
 
 app.listen(port, () => {
