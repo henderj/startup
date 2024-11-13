1fb551e58add10ed27fb8789759a53fbae1d3db9
Josh Hendershot <hendershotjoshr@gmail.com>
2024-11-13 11:25:05 -0700

    build: update deployFiles script

--- deployFiles.sh
+++ deployFiles.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 while getopts k:h:s: flag
 do
     case "${flag}" in
@@ -11,19 +9,32 @@ done
 
 if [[ -z "$key" || -z "$hostname" || -z "$service" ]]; then
     printf "\nMissing required parameter.\n"
-    printf "  syntax: deployFiles.sh -k <pem key file> -h <hostname> -s <service>\n\n"
+    printf "  syntax: deployReact.sh -k <pem key file> -h <hostname> -s <service>\n\n"
     exit 1
 fi
 
-printf "\n----> Deploying files for $service to $hostname with $key\n"
+printf "\n----> Deploying React bundle $service to $hostname with $key\n"
 
 # Step 1
-printf "\n----> Clear out the previous distribution on the target.\n"
+printf "\n----> Build the distribution package\n"
+rm -rf build
+mkdir build
+npm install # make sure vite is installed so that we can bundle
+npm run build # build the React front end
+cp -rf dist/* build # move the React front end to the target distribution
+
+# Step 2
+printf "\n----> Clearing out previous distribution on the target\n"
 ssh -i "$key" ubuntu@$hostname << ENDSSH
 rm -rf services/${service}/public
 mkdir -p services/${service}/public
 ENDSSH
 
-# Step 2
-printf "\n----> Copy the distribution package to the target.\n"
-scp -r -i "$key" * ubuntu@$hostname:services/$service/public
+# Step 3
+printf "\n----> Copy the distribution package to the target\n"
+scp -r -i "$key" build/* ubuntu@$hostname:services/$service/public
+
+# Step 5
+printf "\n----> Removing local copy of the distribution package\n"
+rm -rf build
+rm -rf dist
